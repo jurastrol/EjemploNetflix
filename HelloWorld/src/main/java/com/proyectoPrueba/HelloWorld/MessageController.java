2536cd16bb9854a04bbee90f5fa6ec345401a2ce
“jurastrol” <jurastrol@alumnos.unex.es>
2017-12-09 21:15:52 +0100

    hola

--- HelloWorld/src/main/java/com/proyectoPrueba/HelloWorld/MessageController.java
+++ HelloWorld/src/main/java/com/proyectoPrueba/HelloWorld/MessageController.java
@@ -1,21 +1,17 @@
 package com.proyectoPrueba.HelloWorld;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@EnableAutoConfiguration
 public class MessageController {
 	
-  @Value("${nombre}")
-  private String nombre;
+  private String Nombre = "Juanlu";
 
   @RequestMapping("/")
   ResponseEntity<Message> home() {
-    return new ResponseEntity( new Message("Hello World!"+ nombre) , HttpStatus.ACCEPTED);
+    return new ResponseEntity( new Message("Hello World!"+ Nombre) , HttpStatus.ACCEPTED);
   }
 }
